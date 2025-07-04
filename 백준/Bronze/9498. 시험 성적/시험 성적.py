2f65eb5b61582f752ff57117775ab380b141501e
Dabom Kim <166046744+dabom10@users.noreply.github.com>
2025-07-04 15:14:14 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+score = int(input())
+
+if (89 < score & score < 101):
+    print('A')
+elif (79 < score & score < 90):
+    print('B')
+elif (69 < score & score < 80):
+    print('C')
+elif (59 < score & score < 70):
+    print('D')
+else:
+    print('F')
